46395abbb2e4d39ef52459dad10e684326cea900
mustafamustafa <user@users-MBP.attlocal.net>
2023-05-09 14:24:48 -0400

    results modification

--- mustafaBranch.py
+++ mustafaBranch.py
@@ -2,6 +2,7 @@ import requests
 from bs4 import BeautifulSoup
 import pandas as pd
 from time import sleep
+import re
 
 headers = {
     'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:94.0) Gecko/20100101 Firefox/94.0',
@@ -20,24 +21,27 @@ for i in range(1, 5):
     results = soup.find_all('div', {'class': 's-result-item', 'data-component-type': 's-search-result'})
 
     for result in results:
+        rank_element = result.find('span', {'class': 's-main-slot'})
+        rank = rank_element.text.strip() if rank_element else 'N/A'
+
         product_name = result.h2.text
 
         try:
-            rating = result.find('i', {'class': 'a-icon'}).text
-            rating_count = result.find_all('span', {'aria-label': True})[1].text
+            author = result.find('a', {'class': 'a-size-base a-link-normal'}).text.strip()
         except AttributeError:
-            continue
+            author = 'N/A'
+
+        description = result.find('span', {'class': 'a-size-base-plus a-color-base a-text-normal'}).text.strip()
 
         try:
             price1 = result.find('span', {'class': 'a-price-whole'}).text
             price2 = result.find('span', {'class': 'a-price-fraction'}).text
             price = float(price1 + price2)
             product_url = 'https://amazon.com' + result.h2.a['href']
-            # print(rating_count, product_url)
-            items.append([product_name, rating, rating_count, price, product_url])
+            items.append([rank, product_name, author, description])
         except AttributeError:
             continue
     sleep(1.5)
     
-df = pd.DataFrame(items, columns=['product', 'rating', 'rating count', 'price', 'product url'])
+df = pd.DataFrame(items, columns=['rank', 'product', 'author', 'description'])
 df.to_csv('{0}.csv'.format(search_query), index=False)
